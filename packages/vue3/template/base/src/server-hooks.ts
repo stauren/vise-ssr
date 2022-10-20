19e9793a95a055dc22f0cf5e8d9ff1b3240a4dff
stauren <stauren@qq.com>
2022-10-20 18:49:48 +0800

    add english document for tapable-hooks

--- packages/vue3/template/base/src/server-hooks.ts
+++ packages/vue3/template/base/src/server-hooks.ts
@@ -1,6 +1,9 @@
 import {
   ViseHooks,
   SsrFetchResultOf,
+} from 'vise-ssr';
+import {
+  mergeConfig,
   RenderResultCategory,
 } from 'vise-ssr';
 import { fetchDataForSsrRender } from '@/utils/request';
@@ -8,37 +11,56 @@ import { State } from '@/store';
 
 type LuckNumFetchResult = SsrFetchResultOf<{ value: number | string }>;
 
-// server hooks 具体用法请参考官网: https://stauren.github.io/vise-ssr/
+/**
+ * All hook callbacks could be function or array of functions
+ * Notice: request.url will only include relative path without appName and domain. For example,
+ * dev path: http://127.0.0.1:3000/page-a/
+ * online path: https://example.com/path/to/app-name/page-a/
+ * all match to request.url === '/page-a/'
+ * check https://stauren.github.io/vise-ssr/ for more
+ */
 const serverHooks: ViseHooks = {
-  // app 名称，注意不要包含 app- 的前缀
+  // app name should not have app- prefix
   appName: '<!--ssr-server-hooks-app-name-->',
 
-  // 如果加载 Vise plugin，可以放入此数组
+  // Vise hooks plugin
   plugins: [],
 
-  // receiveRequest:接收到 HTTP 请求后，可以在此拦截请求，可以简单生成 RenderType 为 receiveRequest 的 RenderResult
+  /**
+   * HTTP requests could be intercepted in this hook after server received it from clients
+   * Bypass the default SSR is possible by return a custom `RenderResult`
+   * Marks and data should be put in renderResult.context.extra
+   */
   receiveRequest: async (/* httpRequest */) => {
-    // 如果希望拦截请求，返回 Omit<RenderResult, 'type' | 'renderBy'>
+    // return Omit<RenderResult, 'type' | 'renderBy'> to intercept
   },
 
-  // HTTP 请求解析完成，多个钩子函数顺序执行传递解析结果，可在此修改解析结果
+  /**
+   * Tapped functions receive a `ResolveRequest` and return a `ResolveRequest`.
+   * Marks could be added to context of certain requests for later process.
+   * Be careful of [hydration mismatch] if you change data in the HTTPRequest.
+   */
   requestResolved: async resolvedRequest => resolvedRequest,
 
-  // 在开始使用 HTML 缓存之前执行，如果确定缓存是否过期需要请求接口，可以在这里请求
-  beforeUseCache: async (/* renderRequest */) => {
-    // 返回值类型 CacheInfo（包含 cache key、cache 有效期信息）: void
+  /**
+   * Tapped function calculate `CacheInfo` from `RenderContext`
+   * Which will be used to find previous cached SSR data or save newly generated SSR data.
+   */
+  beforeUseCache: async (renderRequest) => {
   },
 
-  // 在 HTML 缓存命中后并行执行所有钩子，然后响应 HTTP 请求，无法在此更改响应，可做统计等
-  hitCache: async (/* hitCache */) => {
-    // console.log(`Use cache with key: ${hitCache.key}`);
+  // Tapped functions will be notified a successful cache hit event.
+  hitCache: async (hitCache) => {
   },
 
-  // 在准备使用 Vue render bundle 等服务端渲染包生成 HTML 之前调用
+  /**
+   * Tapped functions will be called one by one before rendering HTML with server renderer provided by web UI libraries
+   * Typically this could be used to fetch data for SSR. Data should be transferred in `RenderContext.extra`
+   */
   beforeRender: async (renderContext) => {
     const { url } = renderContext.request;
     let extraInitState = {};
-    // 为首页请求额外接口数据
+    // request data for index page
     if (url === '/') {
       try {
         const apiResult = await fetchDataForSsrRender() as LuckNumFetchResult;
@@ -56,44 +78,49 @@ const serverHooks: ViseHooks = {
         };
       }
     }
-    // 示例使用 strictInitState false 模式，render 期间可以更新 state
+    // strictInitState set to false, state could be updated during render
     const initState: State = {
       count: 0,
       luckyNumber: -1,
       ...extraInitState,
     };
-    return {
-      ...renderContext,
+    return mergeConfig(renderContext, {
       extra: {
-        ...renderContext.extra,
         initState,
       },
-    };
+    });
   },
 
-  // 在 app HTML 渲染完成后（不是整个页面）执行
-  // 根据渲染成功或失败，可能接受 RenderError 或 RenderDone 参数
-  // 如需重载渲染结果，钩子需要返回 RenderResult 重载，瀑布流顺序执行
-  // 注意，如果在这个钩子里面重载渲染结果，在 hydration 的时候可能会发生 mismatch
+  /**
+   * Tapped functions will be called one by one after render finishes
+   * Processing the result of a successful rendering or handle the error of a failed rendering.
+   * Failed SSR could be downgraded to CSR here.
+   * Be careful of [hydration mismatch].
+   */
   afterRender: [async renderResult => renderResult,
-    // 支持数组传入多个 callback
+    // multiple callbacks supported
     {
-      enforce: 'post', // 如有需求，可强制 callback 靠前或靠后执行
-      // 可在此修改渲染结果
+      enforce: 'post', // enforce execute order
       callback: async renderResult => renderResult,
     }],
 
-  // 在所有 HTTP 响应发送前执行
+  /**
+   * Tapped functions will be called after `afterRender`
+   * This is the last hook before HTTP Response is sent
+   * and it's mainly used for compose the HTTP Response
+   * Which could be generated with all data in the
+   * `RenderResult` which contains the `RenderContext`
+   */
   beforeResponse: async (renderResult) => {
-    // 为成功渲染页面，统一添加特定 headers 等
+    // successful render
     if (renderResult.type === RenderResultCategory.render) {
     }
 
-    // 被 receiveRequest hook 拦截的请求，可以被再次拦截
+    // intercepted by receiveRequest hook
     if (renderResult.type === RenderResultCategory.receiveRequest) {
     }
 
-    // 发生服务端异常，可以在此从 renderResult.context 和 renderResult.error
+    // render error happened, get detail in renderResult.context and renderResult.error
     if (renderResult.type === RenderResultCategory.error) {
     }
   },
